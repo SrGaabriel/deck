68c63f41429bf4a2c855b3df5bb6f598300cfa27
gaabriel <xgabrieldev@gmail.com>
2022-01-05 14:31:14 -0300

    Add support to roles and permissions

--- common/src/commonMain/kotlin/com.guildedkt/entity/Roles.kt
+++ common/src/commonMain/kotlin/com.guildedkt/entity/Roles.kt
@@ -2,10 +2,47 @@ package com.guildedkt.entity
 
 import com.guildedkt.util.GenericId
 import com.guildedkt.util.GuildedUnknown
+import com.guildedkt.util.Timestamp
 import kotlinx.serialization.Serializable
 
 @Serializable
 @GuildedUnknown
 data class RawRole(
-    val id: GenericId
+    val id: Long,
+    val color: String,
+    val isBase: Boolean,
+    val isDisplayedSeparately: Boolean,
+    val isMentionable: Boolean,
+    val isSelfAssignable: Boolean,
+    val name: String,
+    val createdAt: Timestamp,
+    val discordRoleId: Long? = null,
+    val discordSyncedAt: Timestamp? = null,
+    val priority: Int,
+    val botScope: Boolean? = null,
+    val teamId: GenericId,
+    val updatedAt: Timestamp,
+    val permissions: RawRolePermissions
+)
+
+@Serializable
+data class RawRolePermissions(
+    val announcements: Int,
+    val bots: Int,
+    val brackets: Int,
+    val calendar: Int,
+    val chat: Int,
+    val customization: Int,
+    val docs: Int,
+    val forms: Int,
+    val forums: Int,
+    val general: Int,
+    val lists: Int,
+    val matchmaking: Int,
+    val media: Int,
+    val recruitment: Int,
+    val scheduling: Int,
+    val streams: Int,
+    val voice: Int,
+    val xp: Int
 )
