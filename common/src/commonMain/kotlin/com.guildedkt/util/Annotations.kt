41c28cbb77f5b0fff5004a76b179840f032473ea
Gabriel <gabriel@lunala.xyz>
2022-01-05 16:32:39 -0300

    Setup CI with GitHub Actions (#2)
    * Create gradle workflow
    
    * Upgrade gradle version and fix errors

--- common/src/commonMain/kotlin/com.guildedkt/util/Annotations.kt
+++ common/src/commonMain/kotlin/com.guildedkt/util/Annotations.kt
@@ -5,7 +5,6 @@ package com.guildedkt.util
  * to the platform. In other words, it's useless and should not be used unless you know what you're doing.
  */
 @Retention(AnnotationRetention.SOURCE)
-@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.CLASS, AnnotationTarget.VALUE_PARAMETER)
 annotation class GuildedUnknown
 
@@ -14,6 +13,5 @@ annotation class GuildedUnknown
  * meaning that it's known but was not adapted and isn't ready for use.
  */
 @Retention(AnnotationRetention.SOURCE)
-@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
 @Target(AnnotationTarget.PROPERTY)
 annotation class LibraryUnsupported
