f492bba4c675b98a6a190e8a7b92a53d23d2a6c4
xGabrielDEV <58668092+xGabrielDEV@users.noreply.github.com>
2022-01-25 14:17:22 -0300

    Remove gradle releases plugin, since it's not doing shit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,15 @@
-import net.researchgate.release.*
-
 plugins {
     kotlin("jvm") version Dependencies.KotlinVersion
-    id("net.researchgate.release") version "2.6.0"
-
 }
 
 subprojects {
     group = "com.deck"
     version = Dependencies.Version
-    plugins.apply("net.researchgate.release")
 
     tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
         kotlinOptions {
             freeCompilerArgs = freeCompilerArgs + listOf("-Xexplicit-api=strict", "-opt-in=kotlin.RequiresOptIn")
             jvmTarget = "1.8"
-        }
-    }
-    release {
-        failOnUnversionedFiles = false
-        failOnCommitNeeded = false
-        failOnPublishNeeded = false
-        with(propertyMissing("git") as GitAdapter.GitConfig) {
-            requireBranch = """master|development|"""
-        }
-        with(propertyMissing("svn") as SvnAdapter.SvnConfig) {
-            username = System.getProperty("USERNAME") ?: System.getenv("USERNAME")
-            password = System.getProperty("TOKEN") ?: System.getenv("TOKEN")
         }
     }
 }
