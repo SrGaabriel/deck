12d0342ebe2af171c8e0333c9f3d6207d3815aae
SrGaabriel <58668092+xGabrielDEV@users.noreply.github.com>
2022-02-12 20:43:24 -0300

    Add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,28 @@
 plugins {
     kotlin("jvm") version Dependencies.KotlinVersion
     kotlin("plugin.serialization") version Dependencies.KotlinVersion
+    `maven-publish`
 }
 
 subprojects {
     group = "com.deck"
     version = Dependencies.Version
     plugins.apply("org.jetbrains.kotlin.plugin.serialization")
+    apply<MavenPublishPlugin>()
 
     tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
         kotlinOptions {
             freeCompilerArgs = freeCompilerArgs + listOf("-Xexplicit-api=strict", "-opt-in=kotlin.RequiresOptIn")
             jvmTarget = "1.8"
+        }
+    }
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+            }
         }
     }
 }
