9d49cd7a92424b237f4287ca5ef754fc4ea67657
SrGaabriel <58668092+xGabrielDEV@users.noreply.github.com>
2022-04-29 20:00:37 -0300

    fix: build errors (wtf intellij)

--- deck-core/src/main/kotlin/io/github/deck/core/event/Wrapping.kt
+++ deck-core/src/main/kotlin/io/github/deck/core/event/Wrapping.kt
@@ -1,13 +1,13 @@
 package io.github.deck.core.event
 
 import io.github.deck.core.DeckClient
-import io.github.deck.core.event.message.DeckMessageCreateEvent
-import io.github.deck.core.event.message.DeckMessageDeleteEvent
-import io.github.deck.core.event.message.DeckMessageUpdateEvent
+import io.github.deck.core.event.message.MessageCreateEvent
+import io.github.deck.core.event.message.MessageDeleteEvent
+import io.github.deck.core.event.message.MessageUpdateEvent
 import io.github.deck.core.event.server.*
-import io.github.deck.core.event.user.DeckHelloEvent
-import io.github.deck.core.event.webhook.DeckServerWebhookCreateEvent
-import io.github.deck.core.event.webhook.DeckServerWebhookUpdateEvent
+import io.github.deck.core.event.user.HelloEvent
+import io.github.deck.core.event.webhook.ServerWebhookCreateEvent
+import io.github.deck.core.event.webhook.ServerWebhookUpdateEvent
 import io.github.deck.core.util.WrappedEventSupplier
 import io.github.deck.core.util.WrappedEventSupplierData
 import io.github.deck.gateway.event.GatewayEvent
@@ -38,17 +38,17 @@ public class DefaultEventService(private val client: DeckClient) : EventService 
 
     override fun startListening(): Job = client.gateway.on<GatewayEvent> {
         val deckEvent: DeckEvent = when (this) {
-            is GatewayHelloEvent -> DeckHelloEvent.map(client, this)
-            is GatewayServerXpAddedEvent -> DeckServerXpAddEvent.map(client, this)
-            is GatewayChatMessageCreatedEvent -> DeckMessageCreateEvent.map(client, this)
-            is GatewayChatMessageUpdatedEvent -> DeckMessageUpdateEvent.map(client, this)
-            is GatewayChatMessageDeletedEvent -> DeckMessageDeleteEvent.map(client, this)
-            is GatewayServerWebhookCreatedEvent -> DeckServerWebhookCreateEvent.map(client, this)
-            is GatewayServerWebhookUpdatedEvent -> DeckServerWebhookUpdateEvent.map(client, this)
-            is GatewayTeamMemberJoinedEvent -> DeckMemberJoinEvent.map(client, this)
-            is GatewayTeamMemberRemovedEvent -> DeckMemberLeaveEvent.map(client, this)
-            is GatewayTeamMemberBannedEvent -> DeckMemberBanEvent.map(client, this)
-            is GatewayTeamMemberUnbannedEvent -> DeckMemberUnbanEvent.map(client, this)
+            is GatewayHelloEvent -> HelloEvent.map(client, this)
+            is GatewayServerXpAddedEvent -> ServerXpAddEvent.map(client, this)
+            is GatewayChatMessageCreatedEvent -> MessageCreateEvent.map(client, this)
+            is GatewayChatMessageUpdatedEvent -> MessageUpdateEvent.map(client, this)
+            is GatewayChatMessageDeletedEvent -> MessageDeleteEvent.map(client, this)
+            is GatewayServerWebhookCreatedEvent -> ServerWebhookCreateEvent.map(client, this)
+            is GatewayServerWebhookUpdatedEvent -> ServerWebhookUpdateEvent.map(client, this)
+            is GatewayTeamMemberJoinedEvent -> MemberJoinEvent.map(client, this)
+            is GatewayTeamMemberRemovedEvent -> MemberLeaveEvent.map(client, this)
+            is GatewayTeamMemberBannedEvent -> MemberBanEvent.map(client, this)
+            is GatewayTeamMemberUnbannedEvent -> MemberUnbanEvent.map(client, this)
             else -> return@on
         }
         eventWrappingFlow.emit(deckEvent)
