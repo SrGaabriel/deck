4205e85a0ab5a8427d9f1b4b7ada44d28adcaaae
Gabriel <xgabrieldev@gmail.com>
2022-08-01 21:10:14 -0300

    chore: delete unused class

--- deck-common/src/jvmMain/kotlin/JvmUuid.kt
+++
@@ -1,9 +0,0 @@
-package io.github.deck.common.util
-
-import java.util.*
-
-public fun UniqueId: UUID =
-    UUID.fromString(raw)
-
-public fun UUID.mapToModel(): UniqueId =
-    UniqueId(toString())
